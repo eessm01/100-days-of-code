a3d0d26b7f792e04bd66fba344c9e208fda7beaa
eessm01 <eessm01@gmail.com>
2020-01-30 05:44:18 -0600

    Day 1. smallest positive number that is evently divisible by all of the numbers from 1 to 20

---
+++ project_euler/5_smallest_multiple.py
@@ -0,0 +1,23 @@
+def get_smallest_multiple(max_divisor):
+    is_find = False
+    initial_smallest_number = 2520
+
+    while not is_find:
+        # print(initial_smallest_number)
+        for i in range(max_divisor, 1, -1):
+            if initial_smallest_number % i > 0:
+                # print(i)
+                break
+            else:
+                if i == 2:
+                    is_find = True
+                    return initial_smallest_number
+
+        initial_smallest_number += 1
+
+
+if __name__ == '__main__':
+    max_divisor = int(input('Ingrese el número máximo:'))
+    initial_smallest_number = get_smallest_multiple(max_divisor)
+    print(initial_smallest_number)
+
